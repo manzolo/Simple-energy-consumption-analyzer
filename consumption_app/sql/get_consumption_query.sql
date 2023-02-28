f98ad77c555fd4065cd37be282c529966c8d73e4
Andrea Manzi <manzolo@libero.it>
2023-02-28 13:53:18 +0100

    Update get_consumption_query.sql

--- consumption_app/sql/get_consumption_query.sql
+++ consumption_app/sql/get_consumption_query.sql
@@ -3,4 +3,5 @@ SELECT c.year, c.month, CAST(c.kwh AS INT) Kwh, CAST(c.smc AS INT) Smc,
        cost.kwh_cost kwh_unit_cost, cost.smc_cost smc_unit_cost, c.id_consumption id
 FROM consumption c
 LEFT JOIN cost ON c.year || '-' || printf('%02d', c.month) || '-01' BETWEEN cost.start AND COALESCE(cost.end, date('now'))
+WHERE c.year >= strftime('%Y', date('now', '-3 years'))
 ORDER BY YEAR DESC, MONTH DESC;
