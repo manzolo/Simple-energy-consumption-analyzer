ba071c990b6bcef5b901196cb5019624989f8811
Andrea Manzi <manzolo@libero.it>
2023-02-28 13:57:28 +0100

    Update queries.py

--- consumption_app/database/queries.py
+++ consumption_app/database/queries.py
@@ -3,15 +3,21 @@ from config import app_dir
 
 def get_consumption_query():
     # read the SQL queries from queries.sql
-    with open(app_dir+'/sql/get_consumption_query.sql', 'r') as f:
+    with open(app_dir + '/sql/get_consumption_query.sql', 'r') as f:
+        return f.read()
+
+
+def get_year_consumption_query():
+    # read the SQL queries from queries.sql
+    with open(app_dir + '/sql/get_year_consumption_query.sql', 'r') as f:
         return f.read()
 
 
 def create_consumption_table():
-    with open(app_dir+'/sql/create_consumption_table.sql', 'r') as f:
+    with open(app_dir + '/sql/create_consumption_table.sql', 'r') as f:
         return f.read()
 
 
 def create_cost_table():
-    with open(app_dir+'/sql/create_cost_table.sql', 'r') as f:
+    with open(app_dir + '/sql/create_cost_table.sql', 'r') as f:
         return f.read()
