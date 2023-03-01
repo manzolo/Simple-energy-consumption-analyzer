74a1ba8e979c2beb0a38f5eceb8506bfd27ea728
Andrea Manzi <manzolo@libero.it>
2023-03-01 20:19:30 +0100

    Refactor crud functions

--- consumption_app/static/js/crud/utility.js
+++ consumption_app/static/js/crud/utility.js
@@ -2,8 +2,8 @@ const tableBody = document.getElementById('table-body');
 const paginationContainer = document.getElementById('pagination-container');
 const urlParams = new URLSearchParams(window.location.search);
 
-function fetchData(endpoint, page = 1, pageSize = 10) {
-  getData(endpoint, renderData, page, pageSize);
+async function fetchData(endpoint, page = 1, pageSize = 10) {
+  const [data, status, headers] = await getData(endpoint, renderData, page, pageSize);
 }
 
 function fetchData(endpoint, page = 1, pageSize = 10) {
@@ -12,39 +12,33 @@ function fetchData(endpoint, page = 1, pageSize = 10) {
   getData(endpoint, renderData, page, pageSize);
 }
 
-function getData(endpoint, renderFunction, page, pageSize) {
-  fetch(`/${endpoint}?page=${page}&page_size=${pageSize}`)
-    .then(response => {
-      // Get total count from response header
-      const total_count = parseInt(response.headers.get('X-Total-Count'));
-      const page_size = parseInt(response.headers.get('X-Page-Size'));
-      const current_page = parseInt(response.headers.get('X-Current-Page'));
-      const total_pages = parseInt(response.headers.get('X-Total-Pages'));
+async function getData(endpoint, renderFunction, page, pageSize) {
+  const response = await fetch(`/${endpoint}?page=${page}&page_size=${pageSize}`);
 
-      // Add pagination metadata to response headers
-      const response_headers = {
-        'X-Total-Count': total_count.toString(),
-        'X-Total-Pages': total_pages.toString(),
-        'X-Current-Page': current_page.toString(),
-        'X-Page-Size': page_size.toString()
-      };
+  const total_count = parseInt(response.headers.get('X-Total-Count'));
+  const page_size = parseInt(response.headers.get('X-Page-Size'));
+  const current_page = parseInt(response.headers.get('X-Current-Page'));
+  const total_pages = parseInt(response.headers.get('X-Total-Pages'));
 
-      return response.json().then(data => {
-        if (data.length > 0) {
-          // Render table rows
-          let html = '';
-          data.forEach(item => {
-            html += renderFunction(item);
-          });
-          tableBody.innerHTML = html;
-        }
-        const paginationHtml = generatePaginationLinks(current_page, total_count, page_size);
-        paginationContainer.innerHTML = `<ul class="pagination">${paginationHtml}</ul>`;
-        // Return data and response headers
-        const result = { data, pagination: response_headers };
-        return [result, 200, response_headers];
-      });
-    });
+  const response_headers = {
+    'X-Total-Count': total_count.toString(),
+    'X-Total-Pages': total_pages.toString(),
+    'X-Current-Page': current_page.toString(),
+    'X-Page-Size': page_size.toString()
+  };
+
+  const data = await response.json();
+
+  if (data.length > 0) {
+    const renderedData = data.map(item => renderFunction(item)).join('');
+    tableBody.innerHTML = renderedData;
+  }
+
+  const paginationHtml = generatePaginationLinks(current_page, total_count, page_size);
+  paginationContainer.innerHTML = `<ul class="pagination">${paginationHtml}</ul>`;
+
+  const result = { data, pagination: response_headers };
+  return [result, 200, response_headers];
 }
 
 function generatePaginationLinks(currentPage, totalRecords, pageSize) {
