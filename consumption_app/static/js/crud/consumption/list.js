efb47ec5b28fef2e9f83b72fdc75a380ef5ad407
Andrea Manzi <manzolo@libero.it>
2023-03-01 23:14:26 +0100

    Add edit in line

--- consumption_app/static/js/crud/consumption/list.js
+++ consumption_app/static/js/crud/consumption/list.js
@@ -1,10 +1,10 @@
 function renderData(consumption) {
-  return `<tr>
-            <td>${consumption.id}</td>
-            <td>${consumption.year}</td>
-            <td>${consumption.month}</td>
-            <td>${consumption.kwh}</td>
-            <td>${consumption.smc}</td>
+  return `<tr data-id="${consumption.id}">
+            <td contenteditable="false">${consumption.id}</td>
+            <td contenteditable="false">${consumption.year}</td>
+            <td contenteditable="false">${consumption.month}</td>
+            <td contenteditable="false">${consumption.kwh}</td>
+            <td contenteditable="false">${consumption.smc}</td>
             <td>
               <button class="btn btn-sm btn-warning" onclick="editConsumption(${consumption.id})">Edit</button>
               <button class="btn btn-sm btn-danger" onclick="deleteConsumption(${consumption.id})">Delete</button>
@@ -12,7 +12,62 @@ function renderData(consumption) {
           </tr>`;
 }
 
-function createConsumption() {
+function editConsumption(id) {
+  const row = document.querySelector(`tr[data-id="${id}"]`);
+  const editBtn = row.querySelector('.btn-warning');
+
+  // Change Edit button to Save button
+  editBtn.textContent = 'Save';
+  editBtn.classList.remove('btn-warning');
+  editBtn.classList.add('btn-success');
+  editBtn.onclick = saveConsumption;
+
+  // Make cells editable
+  const cells = row.querySelectorAll(`td[contenteditable]`);
+  cells.forEach(cell => {
+    cell.contentEditable = true;
+  });
+}
+
+async function saveConsumption() {
+  const row = this.parentNode.parentNode;
+  const id = row.getAttribute('data-id');
+
+  // Disable editing and change Save button back to Edit button
+  const cells = row.querySelectorAll('td[contenteditable]');
+  cells.forEach(cell => cell.contentEditable = false);
+
+  const saveBtn = row.querySelector('.btn-success');
+  saveBtn.textContent = 'Edit';
+  saveBtn.classList.remove('btn-success');
+  saveBtn.classList.add('btn-warning');
+  saveBtn.onclick = () => editConsumption(id);
+
+  // Retrieve updated values from the cells
+  const year = row.children[1].textContent;
+  const month = row.children[2].textContent;
+  const kwh = parseFloat(row.children[3].textContent);
+  const smc = parseFloat(row.children[4].textContent);
+
+  // Send updated values to the server
+  const response = await fetch(`/consumption/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ year, month, kwh, smc })
+  });
+
+  if (response.ok) {
+    // Reload the data to update the table
+    fetchData('consumption', urlParams.get('page'), urlParams.get('page_size'));
+  } else {
+    // Display an error message
+    alert('Failed to update consumption');
+  }
+}
+
+function addConsumption() {
 location.href='/consumption/create'
 }
 
@@ -35,10 +90,6 @@ function deleteConsumption(id) {
       location.reload();
     })
     .catch(error => console.error(error));
-}
-
-function editConsumption(id) {
-location.href='/consumption/edit/'+id
 }
 
 fetchData('consumption', urlParams.get('page'), urlParams.get('page_size'));
--- consumption_app/static/js/crud/cost/list.js
+++ consumption_app/static/js/crud/cost/list.js
@@ -1,12 +1,12 @@
 function renderData(cost) {
-  return `<tr>
+  return `<tr data-id="${cost.id}">
             <td>${cost.id}</td>
-            <td>${new Date(cost.start).toLocaleDateString('en-GB', {day: '2-digit', month: '2-digit', year: 'numeric'})}</td>
-            <td>${cost.end ? new Date(cost.end).toLocaleDateString('en-GB', {day: '2-digit', month: '2-digit', year: 'numeric'}) : ''}</td>
-            <td>${cost.kwh}</td>
-            <td>${cost.smc}</td>
-            <td>${cost.kwh_cost}</td>
-            <td>${cost.smc_cost}</td>
+            <td contenteditable="false">${new Date(cost.start).toLocaleDateString('en-GB', {day: '2-digit', month: '2-digit', year: 'numeric'})}</td>
+            <td contenteditable="false">${cost.end ? new Date(cost.end).toLocaleDateString('en-GB', {day: '2-digit', month: '2-digit', year: 'numeric'}) : ''}</td>
+            <td contenteditable="false">${cost.kwh}</td>
+            <td contenteditable="false">${cost.smc}</td>
+            <td contenteditable="false">${cost.kwh_cost}</td>
+            <td contenteditable="false">${cost.smc_cost}</td>
             <td>
               <button class="btn btn-sm btn-warning" onclick="editCost(${cost.id})">Edit</button>
               <button class="btn btn-sm btn-danger" onclick="deleteCost(${cost.id})">Delete</button>
@@ -14,11 +14,68 @@ function renderData(cost) {
           </tr>`;
 }
 
+function editCost(id) {
+  const row = document.querySelector(`tr[data-id="${id}"]`);
+  const editBtn = row.querySelector('.btn-warning');
+
+  // Change Edit button to Save button
+  editBtn.textContent = 'Save';
+  editBtn.classList.remove('btn-warning');
+  editBtn.classList.add('btn-success');
+  editBtn.onclick = saveCost;
+
+  // Make cells editable
+  const cells = row.querySelectorAll(`td[contenteditable]`);
+  cells.forEach(cell => {
+    cell.contentEditable = true;
+  });
+}
+
+async function saveCost() {
+  const row = this.parentNode.parentNode;
+  const id = row.getAttribute('data-id');
+
+  // Disable editing and change Save button back to Edit button
+  const cells = row.querySelectorAll('td[contenteditable]');
+  cells.forEach(cell => cell.contentEditable = false);
+
+  const saveBtn = row.querySelector('.btn-success');
+  saveBtn.textContent = 'Edit';
+  saveBtn.classList.remove('btn-success');
+  saveBtn.classList.add('btn-warning');
+  saveBtn.onclick = () => editCost(id);
+
+  // Retrieve updated values from the cells
+  const start = new Date(row.children[1].textContent.split('/').reverse().join('/'));
+  const end = row.children[2].textContent ? new Date(row.children[2].textContent.split('/').reverse().join('/')) : null;
+  const kwh = parseFloat(row.children[3].textContent);
+  const smc = parseFloat(row.children[4].textContent);
+  const kwh_cost = parseFloat(row.children[5].textContent);
+  const smc_cost = parseFloat(row.children[6].textContent);
+
+  // Send updated values to the server
+  const response = await fetch(`/cost/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ start, end, kwh, smc, kwh_cost, smc_cost })
+  });
+
+  if (response.ok) {
+    // Reload the data to update the table
+    fetchData('cost', urlParams.get('page'), urlParams.get('page_size'));
+  } else {
+    // Display an error message
+    alert('Failed to update cost');
+  }
+}
+
 function exportCost() {
 location.href='/cost/export'
 }
 
-function createCost() {
+function addCost() {
 location.href='/cost/create'
 }
 
@@ -32,15 +89,11 @@ function deleteCost(id) {
   })
     .then(response => response.json())
     .then(data => {
-      console.log(data);
       // refresh the table after deletion
-      location.reload();
+      fetchData('cost', urlParams.get('page'), urlParams.get('page_size'));
+      //location.reload();
     })
     .catch(error => console.error(error));
-}
-
-function editCost(id) {
-location.href='/cost/edit/'+id
 }
 
 fetchData('cost', urlParams.get('page'), urlParams.get('page_size'));
